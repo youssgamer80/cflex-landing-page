32d3ee2bc36b1dbc2e64b3761fc3dac0d3d898eb
Jean-Louis <jeanlouisboli@gmail.com>
2022-05-04 10:25:44 +0000

    apply flowbite in CardItem component

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="flex flex-col mr-5">
-        <div >
-           <font-awesome-icon  class="text-orange-600 text-lg" :icon="iconvalue"/>
+        <div class="mb-6 rounded-full bg-slate-50 w-20 p-4 text-center">
+           <font-awesome-icon  class="text-orange-500 text-5xl" :icon="iconvalue"/>
            
         </div>
         <div>
-            <h2 class="font-bold text-black  dark:text-white" >{{ title }}</h2>
-            <h3 class="text-black  dark:text-white">{{subtitle }}</h3>
+            <h2 class="font-extrabold text-black text-2xl dark:text-white mb-6" >{{ title }}</h2>
+            <h3 class="text-black  dark:text-white">{{ subtitle }}</h3>
         </div>
     </div>
 </template>
@@ -19,39 +19,3 @@
 </script>
 
 
-<style scoped>
-    /* #card{
-        display: flex;
-        flex-direction: column;
-        align-content: flex-start;
-        margin: 20px;
-        text-align: center;
-    } */
-
-     /* .icon{
-        color: #FB5A00;
-        font-size: 30px;
-        text-align: center;
-        position: relative;
-        top: 20px;
-    }
-
-    #card_icon{
-        width: 80px;
-        height: 80px;
-        background: #EEF4FF;
-        border-radius: 50%
-    }
-
-   
-    h2{
-     
-        font-size: 24px;
-    }
-
-    h3{
-        width: auto;
-        font-size: 18px;
-        color: #747474;
-    } */
-</style>
