54991bcc3c799bb65bfbdd4426a790b82e38afb3
Jean-Louis <jeanlouisboli@gmail.com>
2022-05-04 19:20:00 +0000

    add  sect3-part2

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col mr-5">
-        <div class="mb-6 rounded-full bg-slate-50 w-20 p-4 text-center">
+        <div class="mb-6 rounded-full bg-slate-50 w-20 p-4 text-center ">
            <font-awesome-icon  class="text-orange-500 text-5xl" :icon="iconvalue"/>
            
         </div>
@@ -9,6 +9,7 @@
             <h3 class="text-black  dark:text-white">{{ subtitle }}</h3>
         </div>
     </div>
+
 </template>
 
 <script>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,41 +1,247 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
- <!-- This example requires Tailwind CSS v2.0+ -->
-<div>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div>
     <div class="bg-white dark:bg-gray-800">
-    
-      <section class="py-16 lg:pt-0 pb-36 lg:pb-28 bg-white dark:bg-gray-900" id="figma">
+      <section
+        class="py-16 lg:pt-0 pb-36 lg:pb-28 bg-white dark:bg-gray-900"
+        id="figma"
+      >
         <div class="max-w-screen-xl px-4 lg:px-12 mx-auto">
-          <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 xl:gap-8 items-center">
+          <div
+            class="grid grid-cols-1 lg:grid-cols-2 gap-6 xl:gap-8 items-center"
+          >
             <div class="mb-5 lg:mb-0">
-           <h1 class="mb-4 text-4xl font-bold text-gray-900 lg:font-extrabold lg:text-7xl lg:leading-tight dark:text-white lg:text-center 2xl:px-30 lg:mb-8">Déplacer vous  avec <span class="text-orange-500">Flex</span>ibilité</h1>
+              <h1
+                class="
+                  mb-4
+                  text-4xl
+                  font-bold
+                  text-gray-900
+                  lg:font-extrabold lg:text-7xl lg:leading-tight
+                  dark:text-white
+                  lg:text-center
+                  2xl:px-30
+                  lg:mb-8
+                "
+              >
+                Déplacer vous avec
+                <span class="text-orange-500">Flex</span>ibilité
+              </h1>
               <!-- <h1
                 class="font-bold lg:font-extrabold text-2xl lg:text-4xl lg:leading-snug text-gray-900 dark:text-white mb-4">
 Déplacer vous  avec Flexibilité </h1> -->
-              <p class="text-gray-500 dark:text-gray-400 font-normal text-lg lg:text-xl mb-6 lg:mb-8">
-                Avec C'Flex vous avez les différentes gares des taxis communaux, les horaires de chargement et les points d’arrêt,  les itinéraires possibles, les moyens de transport possibles (Taxi, Bus, Gbaka...) entre entres votre position et votre destination ainsi que les coûts respectifs.</p>
+              <p
+                class="
+                  text-gray-500
+                  dark:text-gray-400
+                  font-normal
+                  text-lg
+                  lg:text-xl
+                  mb-6
+                  lg:mb-8
+                "
+              >
+                Avec C'Flex vous avez les différentes gares des taxis communaux,
+                les horaires de chargement et les points d’arrêt, les
+                itinéraires possibles, les moyens de transport possibles (Taxi,
+                Bus, Gbaka...) entre entres votre position et votre destination
+                ainsi que les coûts respectifs.
+              </p>
               <div class="items-center lg:flex">
-                <button type="button" class="text-white bg-gradient-to-br from-orange-400 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Pink to Orange</button>
+                <button
+                  type="button"
+                  class="
+                    text-white
+                    bg-gradient-to-br
+                    from-orange-400
+                    to-orange-500
+                    hover:bg-gradient-to-bl
+                    focus:ring-4 focus:outline-none focus:ring-pink-200
+                    dark:focus:ring-pink-800
+                    font-medium
+                    rounded-lg
+                    text-sm
+                    px-5
+                    py-2.5
+                    text-center
+                    mr-2
+                    mb-2
+                  "
+                >
+                  Pink to Orange
+                </button>
 
-                <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-orange-400 to-orange-500 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Pink to orange
-  </span>
-</button></div>
+                <button
+                  class="
+                    relative
+                    inline-flex
+                    items-center
+                    justify-center
+                    p-0.5
+                    mb-2
+                    mr-2
+                    overflow-hidden
+                    text-sm
+                    font-medium
+                    text-gray-900
+                    rounded-lg
+                    group
+                    bg-gradient-to-br
+                    from-orange-400
+                    to-orange-500
+                    group-hover:from-pink-500 group-hover:to-orange-400
+                    hover:text-white
+                    dark:text-white
+                    focus:ring-4 focus:outline-none focus:ring-pink-200
+                    dark:focus:ring-pink-800
+                  "
+                >
+                  <span
+                    class="
+                      relative
+                      px-5
+                      py-2.5
+                      transition-all
+                      ease-in
+                      duration-75
+                      bg-white
+                      dark:bg-gray-900
+                      rounded-md
+                      group-hover:bg-opacity-0
+                    "
+                  >
+                    Pink to orange
+                  </span>
+                </button>
+              </div>
             </div>
             <div>
-              <img class="w-full dark:hidden" src="assets/HeroImage.svg" alt="Data app">
-              <img class="w-full hidden dark:block" src="assets/HeroImage.svg" alt="Dta app dark">
+              <img
+                class="w-full dark:hidden"
+                src="assets/HeroImage.svg"
+                alt="Data app"
+              />
+              <img
+                class="w-full hidden dark:block"
+                src="assets/HeroImage.svg"
+                alt="Dta app dark"
+              />
             </div>
           </div>
         </div>
       </section>
-    
-     <section class="mx-auto  flex justify-center px-6 pt-8 m-8 ">
-        <CardItem iconvalue="calendar-days" title="Set a better deadline tasks" subtitle="You can make you duty deadline better and more orderly" />
-        <CardItem  iconvalue="layer-group" title="Manage the task so easiluy" subtitle="You can make you duty deadline better and more orderly" />
-        <CardItem  iconvalue="bell" title="Wouldn't miss notifications" subtitle="Of course you will always get the task and schedule notifications" />
-     </section>
+
+      <section class="md:flex justify-center px-6 pt-8">
+        <CardItem
+          iconvalue="calendar-days"
+          title="Set a better deadline tasks"
+          subtitle="You can make you duty deadline better and more orderly"
+        />
+        <CardItem
+          iconvalue="layer-group"
+          title="Manage the task so easiluy"
+          subtitle="You can make you duty deadline better and more orderly"
+        />
+        <CardItem
+          iconvalue="bell"
+          title="Wouldn't miss notifications"
+          subtitle="Of course you will always get the task and schedule notifications"
+        />
+      </section>
+
+      <section
+        class="py-16 lg:pt-0 pb-36 lg:pb-28 bg-white dark:bg-gray-900"
+        id="figma"
+      >
+        <div class="max-w-screen-xl px-4 lg:px-12 mx-auto">
+          <div
+            class="grid grid-cols-1 lg:grid-cols-2 gap-6 xl:gap-8 items-center"
+          >
+            <div class="mb-5 lg:mb-0">
+              <h1
+                class="
+                  mb-4
+                  text-4xl
+                  font-bold
+                  text-gray-900
+                  lg:font-extrabold lg:text-2xl lg:leading-tight
+                  dark:text-white
+                  lg:text-center
+                  2xl:px-30
+                  lg:mb-8
+                "
+              >
+                Recevez les notification de YooBi les plus prioritaire
+              </h1>
+
+              <p
+                class="
+                  text-gray-500
+                  dark:text-gray-400
+                  font-normal
+                  text-lg
+                  lg:text-xl
+                  mb-6
+                  lg:mb-8
+                "
+              >
+                The best feature makes you better, with the latest technology to
+                meet your needs. Of course, with such features as organizing
+                schedules and leading assignments, it will make you easier, So
+                get the application now !
+              </p>
+            </div>
+            <div class="">
+              <img
+                class="w-full h-98 p-10 dark:hidden"
+                src="assets/sect3_bottom.svg"
+                alt="Data app"
+              />
+              <img
+                class="w-full h-full hidden dark:block"
+                src="assets/sect3_bottom.svg"
+                alt="Dta app dark"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="max-w-screen-xl px-4 lg:px-12 mx-auto" id="figma">
+        <div class="mb-5 lg:mb-0">
+          <h1
+            class="
+              mb-4
+              text-4xl
+              font-bold
+              text-white-900
+              lg:font-extrabold lg:text-2xl lg:leading-tight
+              dark:text-white
+              lg:text-center
+              2xl:px-30
+              lg:mb-8
+            "
+          >
+            Download an app now and the various benefits you will get
+            immediately
+          </h1>
+
+        </div>
+
+        <div class="">
+          <img
+            class="p-10 dark:hidden"
+            src="assets/User_paging.png"
+            alt="Data app"
+          />
+          <img
+            class="w-full h-full hidden dark:block"
+            src="assets/User_paging.png"
+            alt="Dta app dark"
+          />
+        </div>
+      </section>
 
       <!-- le footer 
       <footer class="py-16 bg-white lg:py-24 dark:bg-gray-800">
@@ -118,18 +324,14 @@ Déplacer vous  avec Flexibilité </h1> -->
         </div>
       </footer>-->
     </div>
-</div>
-
+  </div>
 </template>
 <script>
-
-import CardItem from './CardItem.vue'
+import CardItem from "./CardItem.vue";
 export default {
-  name :"HeroSection",
-  components:{
-    CardItem
-  }
-  }
- 
-  
+  name: "HeroSection",
+  components: {
+    CardItem,
+  },
+};
 </script>
