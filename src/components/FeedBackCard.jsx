b717d9f2da75305731a47ac14552e72f495ac404
baazz20 <yaoeloge@outlook.fr>
2022-09-09 01:36:05 +0000

    Ajout de la nouvel configuration pour le deploiment

--- src/components/FeedBackCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FeedBackCard = () => {
-  return (
-    <div>FeedBackCard</div>
-  )
-}
-
-export default FeedBackCard
--- src/components/Temoignages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Temoignages = () => {
-  return (
-    <div>Temoignages</div>
-  )
-}
-
-export default Temoignages
