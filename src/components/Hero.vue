f505da01b409fa58ed00fa2af9bc91bd4a04f960
baazz20 <yaoeloge@outlook.fr>
2022-09-11 04:36:58 +0000

    fix button burg

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -31,7 +31,7 @@
                         </div>
                         <div>
                             <router-link to="/demande"
-                                class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 logo">Souscrire</router-link>
+                            type="button" class="btn py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 logo">Souscrire</router-link>
                         </div>
                     </div>
                     <div
